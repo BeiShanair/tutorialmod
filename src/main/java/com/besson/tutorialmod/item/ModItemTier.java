d23417bf56acd963c8c2a0e60ee4a07cbf904c68
wzair911 <2430649818@qq.com>
2023-07-04 08:48:04 +0800

    Minecraft 1.16.5 Forge Modding Development

---
+++ src/main/java/com/besson/tutorialmod/item/ModItemTier.java
@@ -0,0 +1,52 @@
+package com.besson.tutorialmod.item;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+    FIRE_ETHER(2,1024,7,3,16,() -> {
+        return Ingredient.fromItems(ModItems.FIRE_ETHER.get());
+    });
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    private ModItemTier(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
+        this.harvestLevel = harvestLevelIn;
+        this.maxUses = maxUsesIn;
+        this.efficiency = efficiencyIn;
+        this.attackDamage = attackDamageIn;
+        this.enchantability = enchantabilityIn;
+        this.repairMaterial = new LazyValue<>(repairMaterialIn);
+    }
+    public int getMaxUses() {
+        return this.maxUses;
+    }
+
+    public float getEfficiency() {
+        return this.efficiency;
+    }
+
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    public int getHarvestLevel() {
+        return this.harvestLevel;
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.getValue();
+    }
+}
--- src/main/java/com/besson/tutorialmod/item/ModItems.java
+++ src/main/java/com/besson/tutorialmod/item/ModItems.java
@@ -3,8 +3,7 @@ package com.besson.tutorialmod.item;
 import com.besson.tutorialmod.TutorialMod;
 import com.besson.tutorialmod.item.custom.FireEther;
 import com.besson.tutorialmod.item.custom.IceEther;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,6 +17,17 @@ public class ModItems {
             () -> new IceEther(new Item.Properties().group(ModItemGroup.TUTORIAL_TAB).maxDamage(8)));
     public static final RegistryObject<Item> FIRE_ETHER = ITEMS.register("fireether",
             () -> new FireEther(new Item.Properties().group(ModItemGroup.TUTORIAL_TAB).maxDamage(8)));
+    public static final RegistryObject<Item> FIRE_ETHER_AXE = ITEMS.register("fireether_axe",
+            () -> new AxeItem(ModItemTier.FIRE_ETHER,5f,-3f,new Item.Properties().group(ModItemGroup.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> FIRE_ETHER_SWORD = ITEMS.register("fireether_sword",
+            () -> new SwordItem(ModItemTier.FIRE_ETHER,3,-2.4f,new Item.Properties().group(ModItemGroup.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> FIRE_ETHER_PICKAXE = ITEMS.register("fireether_pickaxe",
+            () -> new PickaxeItem(ModItemTier.FIRE_ETHER,1,-2.8f,new Item.Properties().group(ModItemGroup.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> FIRE_ETHER_HOE = ITEMS.register("fireether_hoe",
+            () -> new HoeItem(ModItemTier.FIRE_ETHER,-2,0f,new Item.Properties().group(ModItemGroup.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> FIRE_ETHER_SHOVEL = ITEMS.register("fireether_shovel",
+            () -> new ShovelItem(ModItemTier.FIRE_ETHER,1.5f,-3f,new Item.Properties().group(ModItemGroup.TUTORIAL_TAB)));
+
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }

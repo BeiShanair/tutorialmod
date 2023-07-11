af009a809cbe85ef720d3a8fb86633294e09d21f
wzair911 <2430649818@qq.com>
2023-07-11 09:27:11 +0800

    Minecraft 1.16.5 Forge Modding Development

---
+++ src/main/java/com/besson/tutorialmod/world/ModWorldEvents.java
@@ -0,0 +1,15 @@
+package com.besson.tutorialmod.world;
+
+import com.besson.tutorialmod.TutorialMod;
+import com.besson.tutorialmod.world.gen.ModTreeGeneration;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID)
+public class ModWorldEvents {
+    @SubscribeEvent
+    public static void biomeLoadingEvent(final BiomeLoadingEvent event){
+        ModTreeGeneration.generateTrees(event);
+    }
+}
